c6d986e4e11aa26309689d88ec8208a5c7d94409
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-11-16 13:03:03 +0100

    added two controllers

--- HIMYD_NotOnlyGamesJamEntry/Assets/Scripts/SnakeController.cs
+++ HIMYD_NotOnlyGamesJamEntry/Assets/Scripts/SnakeController.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class SnakeController : MonoBehaviour
 {
     public float moveSpeed = 2.5f;
 
+    //CONTROLLER
+    PlayerIndex playerIndex;
+    GamePadState state;
+    private void Start()
+    {
+        playerIndex = PlayerIndex.Two;
+
+    }
+
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        state = GamePad.GetState(playerIndex);
+        Vector3 movement = new Vector3(state.ThumbSticks.Left.X, 0f, state.ThumbSticks.Left.Y);
         if (movement != Vector3.zero)
         {
             transform.position += movement * moveSpeed * Time.deltaTime;
