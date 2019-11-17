831f9ed619e8d64bcfcac00055ca29329b79375d
Acaree <acampamar@gmail.com>
2019-11-17 12:22:22 +0100

    Now flamingo makes noise

---
+++ HIMYD_NotOnlyGamesJamEntry/Assets/flamingo_sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flamingo_sound : MonoBehaviour
+{
+    
+    private float sound_time = 5.0f;
+    private float sound_timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        sound_timer += Time.deltaTime;
+
+        if(sound_timer >= sound_time)
+        {
+            sound_timer = 0.0f;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+
+    }
+}
