ce736c47fe18c392e05785136667b12ef6748d33
DavidTheMaaster <davidthegaamer@gmail.com>
2019-11-17 12:02:55 +0100

    Added Main Menu

---
+++ HIMYD_NotOnlyGamesJamEntry/Assets/ChangeMainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using XInputDotNetPure;
+public class ChangeMainMenu : MonoBehaviour
+{
+    public PlayerIndex playerIndex;
+    GamePadState state;
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+      
+    }
+
+    void Update()
+    {
+        state = GamePad.GetState(playerIndex);
+        if (state.Buttons.A== ButtonState.Pressed)
+        {
+            ChangeScene();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Level_001");
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
