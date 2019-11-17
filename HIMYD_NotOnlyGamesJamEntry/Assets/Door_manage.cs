c831849d2ba53d86597b730f020e1ff7927af979
Acaree <acampamar@gmail.com>
2019-11-17 09:55:35 +0100

    Buttons working

--- HIMYD_NotOnlyGamesJamEntry/Assets/Door_manage.cs
+++ HIMYD_NotOnlyGamesJamEntry/Assets/Door_manage.cs
@@ -9,6 +9,7 @@ public class Door_manage : MonoBehaviour
     public GameObject button_mole;
     public float distance_movement;
     private float final_pos;
+    public float door_speed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,9 @@ public class Door_manage : MonoBehaviour
     {
         if(button_snake.gameObject.GetComponent<button_manage>().button_pressed && button_mole.gameObject.GetComponent<button_manage>().button_pressed)
         {
-            if(final_pos > gameObject.transform.position.y)
+            if(final_pos < gameObject.transform.position.y)
             {
-                gameObject.transform.position = new Vector3 (gameObject.transform.position.x, 10 * Time.deltaTime, gameObject.transform.position.z);
+                gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y - door_speed * Time.deltaTime, gameObject.transform.position.z);
             }
         }
 
