a010365512f7fce77de086cb07c2ad02282c0e3f
jaume <jaume.montagut.i.guix@gmail.com>
2019-11-17 03:37:19 +0100

    reorganize

---
+++ HIMYD_NotOnlyGamesJamEntry/Assets/Scripts/SnakeTrap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTrap : MonoBehaviour
+{
+    private AudioSource trapSFX;
+    private Transform moleTransform;
+
+    private void Start()
+    {
+        moleTransform = FindObjectOfType<MoleController>().transform;
+        trapSFX = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        
+        if ((int)transform.position.x ==  (int)moleTransform.position.x
+            && (int)transform.position.z == (int)moleTransform.position.z
+            && (int)transform.position.y > (int)moleTransform.position.y)
+        {
+            if (!trapSFX.isPlaying)
+            {
+                trapSFX.Play();
+            }
+        }
+        else
+        {
+            if (trapSFX.isPlaying)
+            {
+                trapSFX.Stop();
+            }
+        }
+    }
+}
